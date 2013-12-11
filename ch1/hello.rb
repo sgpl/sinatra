d07ccad124778d3bfb65084f8d2f3aa26f8e5800
sgpl <sharad45@gmail.com>
2013-12-11 22:02:57 +0300

    edits to file

--- ch1/hello.rb
+++ ch1/hello.rb
@@ -1,5 +1,8 @@
 require 'sinatra' # ruby require statement that pulls in all the code from the Sinatra library
 require 'sinatra/reloader' if development?
+# ^ means that if we add stuff we don't need to restart the sever
+# to see the changes. 
+
 =begin 
 the following block is the "route handler"
 it starts with htTP verb 'get'
@@ -19,3 +22,17 @@ end
 get '/sharad' do
 	"Hello World " * 10 
 end
+
+get '/sharad2' do
+	"Hello World Hello" * 10000 
+end
+
+get '/:name' do 
+	name = params[:name]
+	"Hello #{name}"
+end
+# ^ this route contains a named parameter, called :name, signified
+# by the leading colon. 
+
+# Named parameter can go anywhere in the url and are available in the 
+# handler as part of the params hash. 
