47948c89956deb82607dab99844a15fba33b816f
sgpl <sharad45@gmail.com>
2013-12-12 11:03:36 +0300

    end of ch1, starting the next chapter

--- ch1/hello.rb
+++ ch1/hello.rb
@@ -32,10 +32,17 @@ get '/:name' do
 	"Hello #{name}"
 end
 
+
+# ^ this route contains a named parameter, called :name, signified
+# by the leading colon. 
+
+# Named parameter can go anywhere in the url and are available in the 
+# handler as part of the params hash. 
+
 get '/:one/:two/:three' do
 	"first: #{params[:one]}, second: #{params[:two]}, third: #{params[:three]}"
 end
-
+# ^ pretty self explainatory no? 
 
 get '/what/time/is/it/in/:number/:hours' do #  this is the same thing as :name
 	number = params[:number].to_i
@@ -43,8 +50,7 @@ get '/what/time/is/it/in/:number/:hours' do #  this is the same thing as :name
 	"The time in #{number} hours will be #{time.strftime('%I:%M %p')}"
 end
 
-# ^ this route contains a named parameter, called :name, signified
-# by the leading colon. 
-
-# Named parameter can go anywhere in the url and are available in the 
-# handler as part of the params hash. 
+# understand what time is doing: 
+# Time.now displays current time
+# we can add seconds to Time.now hence the * 3600 (60 min * 60 sec)
+# 
