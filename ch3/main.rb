b29afd07e171ca2af5289f3dd7d1bfca1ca06092
sgpl <sharad45@gmail.com>
2013-12-13 16:18:54 +0300

    deleted ch2/public/styles.css; duplicated the ch2 folder as ch3 to add songs to the web app; got the songs database to run using sqlite and data_manager; need to debug a bit, but commiting changes till then.

---
+++ ch3/main.rb
@@ -0,0 +1,63 @@
+require 'sass'
+require 'sinatra'
+require 'slim'
+require 'sinatra/reloader' if development?
+require './song'
+
+#set :public_folder, 'assets' # default name is public, but you can set it to whatever you want. 
+#set :views, 'templates'
+
+# the default names are public and views
+# if you want to change them, you need to use the above code. 
+
+
+get ('/styles.css') { scss :styles } 
+
+# do = { and end = } in this block.
+
+get '/' do 
+	@title = "Homepage. This is the homepage for this website!!!"
+	slim :home # this is known as a view. 
+			  # just like route handlers, except that instead of finishing with a string that's  
+			  # sent back to the browser, we finish with erb :home
+			  # it's a reference to what is known as a view (which is a representation of data, such as markup )
+end
+
+get '/about' do 
+	@title = "This is the TITLE for the about page"
+	slim :about		
+end
+
+get '/contact' do 
+	slim :contact
+end
+
+not_found do 
+	@title = "You typed something wrong in the browser! ha ha ha"
+	slim :not_found
+end
+
+=begin
+ # can also do this: // also need to create a four_o_four.erb in views
+four_o_four do
+	erb :four_o_four
+end
+=end
+
+get 'fake_error' do
+	status 500 # see wikipedia List of HTTP status codes (for a more comprehensive list of codes)
+	"This is a fake error mate!"
+end
+
+
+get '/songs' do
+	@songs = Song.all
+	slim :songs
+end
+
+
+get '/songs/:id' do 
+	@songs = Song.get(params[:id])
+	slim :show_song
+end
+
---
+++ ch3/song.rb
@@ -0,0 +1,43 @@
+require 'dm-core' # main data_mapper gem
+require 'dm-migrations'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+# this will create a file called development.db which will store all 
+# the database information. 
+
+class Song
+	include DataMapper::Resource # this line links song class to DataMapper which inclides the Resource module from the DataMapper gem as a mixin. 
+	# this is how you make a ruby class a DataMapper resource. 
+	property :id, Serial 
+	property :title, String 
+	property :lyrics, Text 
+	property :length, Integer 
+	property :released_on, Date 
+end
+
+DataMapper.finalize
+# DataMapper.finalize is required after all classes using DataMapper to check their integrity. 
+
+
+=begin 
+DataMapper properties all have a type, which correspond to core ruby classes. 
+String - short strings
+Text - longer pieces of text
+Integer - used for whole nos. 
+Float - for float
+Boolean - for boolean values. 
+DateTime - for date and time
+=end
+
+
+# copying the following two get methods because songs/:id isn't getting displayed properly
+get '/songs' do
+	@songs = Song.all
+	slim :songs
+end
+
+
+get '/songs/:id' do 
+	@songs = Song.get(params[:id])
+	slim :show_song
+end
